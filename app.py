fa852e809aa92d19e6f6c6470b82cf8010b87f8b
Mayur Dhoble <mayurdhoble5@gmail.com>
2024-07-09 18:05:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ import numpy as np
 model=pickle.load(open('lgrmodelf.pkl','rb'))
 car=pd.read_csv(r'file:///C:\Users\dell\Downloads\Cleaned_Car_data.csv')
 
-
+st.title('Car Price Prediction')
 
 # Assuming you have a loaded prediction model named 'model'
 
-#companies = ['Hyundai', 'Mahindra', 'Ford', 'Maruti', 'Skoda', 'Audi', 'Toyota',
-   #    'Renault', 'Honda', 'Datsun', 'Mitsubishi', 'Tata', 'Volkswagen',
-    #   'Chevrolet', 'Mini', 'BMW', 'Nissan', 'Hindustan', 'Fiat', 'Force',
-    #   'Mercedes', 'Land', 'Jaguar', 'Jeep', 'Volvo']
-
-
-#years = [2015, 2016, 2017, 2018, 2019, 2020]
 for companies in [car['company']]:
   selected_company = st.selectbox("Select the company:", companies)
 selected_names = car.iloc[:, 1]  # Assuming "name" is the second column (index 1)
@@ -33,17 +26,11 @@ for fuel_types in [car['fuel_type']]:
   selected_fuel_type = st.selectbox("Select the fuel type:", fuel_types)
   
 
-#fuel_types = ["Petrol", "Diesel", "CNG"]
-
-#selected_company = st.selectbox("Select the company:", companies)
-#selected_year = st.selectbox("Select Year of Purchase:", years)
-#km_driven = st.number_input("Enter the Number of Kilometres driven:")
-#selected_fuel_type = st.selectbox("Select the Fuel Type:", fuel_types)
 
 def predict_price():
   # Prepare user input data as a DataFrame
   data = np.array([selected_company,selected_model, selected_year, km_driven, selected_fuel_type])
-  #df = pd.DataFrame(columns=['company','names' ,'year', 'kms_driven', 'fuel_type'], data=[data])
+  
 
   # Make prediction using the model
   prediction = model.predict(data)
